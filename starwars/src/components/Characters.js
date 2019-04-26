04aa874c7a2184dff2f7d9f9c1a2305c4c389d93
Roger Caballero <rogercp1234@yahoo.com>
2019-04-26 08:23:54 -0700

    imports test working

---
+++ starwars/src/components/Characters.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Characters.css';
+
+const  Character = props=> {
+    return (
+       <div>
+           testing
+       </div>
+    
+    );
+}
+export default Character;
