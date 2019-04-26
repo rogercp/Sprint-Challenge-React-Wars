04ee721b241320fe1692affd0385b29e6b34ffec
Roger Caballero <rogercp1234@yahoo.com>
2019-04-26 09:08:38 -0700

    cards styled

--- starwars/src/components/Characters.js
+++ starwars/src/components/Characters.js
@@ -9,15 +9,15 @@ const  Character = props=> {
                 <p>height: {props.character.height}</p>  
                 <p>mass: {props.character.mass}</p>   
                 <p>hair_color: {props.character.hair_color}</p>  
-                <p>skin_color:{props.character.skin_color}</p>  
+                <p>skin_color: {props.character.skin_color}</p>  
                 <p>eye_color: {props.character.eye_color}</p>  
                 <p>birth_year: {props.character.birth_year}</p>  
                 <p>gender: {props.character.gender}</p>   
                 <p>homeworld: {props.character.homeworld}</p> 
-                <p>films: {props.character.films}</p>  
-                <p>species: {props.character.species}</p>  
-                <p>vehicles: {props.character.vehicles}</p>  
-                <p>starships: {props.character.starships}</p>  
+                <p >films: {props.character.films}</p>  
+                <p >species: {props.character.species}</p>  
+                <p >vehicles: {props.character.vehicles}</p>  
+                <p >starships: {props.character.starships}</p>  
                 <p>created: {props.character.created}</p> 
                 <p>edited: {props.character.edited}</p>  
                 <p>url: {props.character.url}</p>  
